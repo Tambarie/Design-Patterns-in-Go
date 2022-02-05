b08ae2eed148a908a8dc437215a532554f0e30a1
tambarie <gt.gbaragbo@gmail.com>
2022-02-05 08:50:57 +0100

    modified

--- OpenClosePrinciple/main.go
+++ OpenClosePrinciple/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 //OCP
 // Open for extension, closed for modification
 //Specification
@@ -25,13 +27,47 @@ type Product struct {
 }
 
 type Filter struct {
-	
+	//
 }
 
-func (f *Filter) FilterByColor(product []Product, color Color) *Product {
+func (f *Filter) FilterByColor(products []Product, color Color) []*Product {
+	result := make([]*Product,0)
+	for i, product := range products {
+		if product.color == color{
+			result = append(result,&products[i])
+		}
+	}
+
+	return result
 	
 }
 
 func main()  {
-	
+	apple := Product{
+		name:  "Apple",
+		color: green,
+		size:  small,
+	}
+
+	tree := Product{
+		name:  "Tree",
+		color: red,
+		size:  medium,
+	}
+	house := Product{
+		name:  "House",
+		color: blue,
+		size:  large,
+	}
+
+	products := []Product{apple,tree,house}
+
+
+	fmt.Printf("Green products (old): \n")
+	f := Filter{}
+
+	for _, v := range f.FilterByColor(products,green) {
+		fmt.Printf("- %s is green \n",v.name)
+	}
+
 }
